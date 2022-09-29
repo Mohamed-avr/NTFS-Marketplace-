5b5bfbfe52cc44a5368e374de678f8f960727a6d
mohamed <mouh4945@gmail.com>
2022-09-29 22:57:42 +0100

    ADD

--- components/Counter.js
+++ components/Counter.js
@@ -1,6 +1,6 @@
 const Counter = () => {
   return (
-    <article className=" text-lg absolute  from-purple-600 to-purple-500 bg-graident-t-l py-2 px-4 -top-[64px] left-0 ">
+    <article className=" text-lg absolute bg-gradient-to-r  from-purple-700 to-purple-400/90  py-2 px-4 -top-[64px] left-0 ">
       02:23:23
     </article>
   );
