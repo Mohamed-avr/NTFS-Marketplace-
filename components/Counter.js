ec120d280dc347f0259d0eb48068f63b8dfe3165
mohamed <mouh4945@gmail.com>
2022-09-30 22:49:22 +0100

    ADD

--- components/Counter.js
+++ components/Counter.js
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
 const Counter = () => {
+
+   let [ seconds , setSecounds] = useState(10)
+   let [ minutes , setMinutes] = useState(20)
+   let [ hours , setHours] = useState(2)
+
+
+   setInterval(() => {
+ 
+   }, 1000);
+
   return (
     <article className=" text-lg absolute bg-gradient-to-r  from-purple-700 to-purple-400/90  py-2 px-4 -top-[64px] left-0 ">
-      02:23:23
+    {hours}:{minutes}:{seconds}
     </article>
   );
 };
