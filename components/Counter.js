a0e3093ac6ee98f1c0a8f87d329648697a75f4e1
mohamed <mouh4945@gmail.com>
2022-10-09 12:28:04 +0100

    ADD

--- components/Counter.js
+++ components/Counter.js
@@ -1,10 +1,28 @@
 import { useState } from "react";
 
 const Counter = () => {
+  
+  let [ seconds , setSecounds] = useState(10)
+  let [ minutes , setMinutes] = useState(10)
+  let [ hours , setHours] = useState(2)
+  let [remSeconds , seTRemSeconds] = useState(10)
 
-   let [ seconds , setSecounds] = useState(10)
-   let [ minutes , setMinutes] = useState(20)
-   let [ hours , setHours] = useState(2)
+  const secondPass = () => {
+        setMinutes( () => {
+          Math.floor(seconds / 60)
+        
+        }) 
+        seTRemSeconds( () => {
+          seconds % 60
+        })}
+
+        setInterval(function() {
+        
+          secondPass();
+      }, 1000);
+
+
+
 
 
    setInterval(() => {
@@ -13,15 +31,9 @@ const Counter = () => {
 
   return (
     <article className=" text-lg absolute bg-gradient-to-r  from-purple-700 to-purple-400/90  py-2 px-4 -top-[64px] left-0 ">
-    {hours}:{minutes}:{seconds}
+   {minutes}:{seconds}
     </article>
   );
 };
 
 export default Counter;
-
-// empty comment 
-
-const afunction = (a , b) => {
-  console.log(2*b)
-}
