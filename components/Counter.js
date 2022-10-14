61b18b0adb599b2ff315b391522136182e58e58d
mohamed <mouh4945@gmail.com>
2022-10-14 23:20:34 +0100

    ADD

--- components/Counter.js
+++ components/Counter.js
@@ -1,16 +1,45 @@
 import { useState , useEffect } from "react";
 
-const defaultRemainingTime = {
-  seconds : '00', 
-  minutes : '00', 
-  hours : '00' , 
-  days : '00',
-}
+
+
+
+
+
 
 const Counter = () => {
+
+  // deadline date
+const countDownDate = new Date("Dec 31 , 2022 23:59:59 ")
+const [ days , setDays ] = useState(0);
+const [ hours , sethours ] = useState(0);
+const [ minutes , setminutes ] = useState(0);
+const [ secounds , setsecounds ] = useState(0);
+
+
+let theCounter  = setInterval(() => {
+  // currrent date 
+  const currentDate = new Date().getTime();
+  // diffrent time
+  const diffTime = countDownDate - currentDate ;
+  
+  // get time unites 
+  // 1000ms = 1s
+
+  setDays(Math.floor(diffTime/ (1000*60*60*24)))
+  sethours(Math.floor((diffTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)))
+  setminutes(Math.floor((diffTime% (1000 * 60 * 60)) / (1000 * 60)))
+  setsecounds(Math.floor((diffTime  % (1000 * 60)) / 1000) )
+
+}, 1000);
+
+useEffect( () => {
+  theCounter
+})
+
+
   return (
     <article className=" text-lg absolute bg-gradient-to-r  from-purple-700 to-purple-400/90  py-2 px-4 -top-[64px] left-0 ">
-   { '12:12'}
+   { hours.length <= 1 ? `02 ${hours}`: hours} : { minutes.length <= 1 ? `0 ${minutes}`: minutes }: { secounds.length <= 1 ? `0 ${secounds}`: secounds }
     </article>
   );
 };
